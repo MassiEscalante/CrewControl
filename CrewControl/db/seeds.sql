4abe1e03a468104323f1de8f121605bf917cb51a
MassiEscalante <massiel2611@gmail.com>
2024-10-10 13:57:17 -0700

    Added user prompts and routing to the correct queries for index.js

--- CrewControl/db/seeds.sql
+++ CrewControl/db/seeds.sql
@@ -1,15 +1,14 @@
 -- Departments
-INSERT INTO department (name) VALUES ('Engineering'), ('Finance'), ('Sales');
+INSERT INTO department (name) VALUES ('Cardiologist'), ('Airline Pilot'), ('Investment Banker');
 
 -- Roles
 INSERT INTO role (title, salary, department_id) VALUES 
-  ('Software Engineer', 80000, 1),
-  ('Accountant', 70000, 2),
-  ('Sales Manager', 90000, 3);
+  ('Cardiologist', 324760, 1),
+  ('Airline Pilot', 215600, 2),
+  ('Investment Banker', 144633, 3);
 
 -- Employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-  ('John', 'Doe', 1, NULL), 
-  ('Jane', 'Smith', 2, 1), 
-  ('Mike', 'Johnson', 3, NULL);
-  
+  ('Elena', 'Montserrat', 1, NULL), 
+  ('Luz', 'Marisol', 2, 1), 
+  ('Violeta', 'Valentina', 3, NULL);
