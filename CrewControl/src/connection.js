cff0102fc8ea6f8143a20a397ebf72d402ed39de
MassiEscalante <massiel2611@gmail.com>
2024-10-10 17:21:43 -0700

    Used the default import for pg and then destructured the Pool object from it in connection.js

--- CrewControl/src/connection.js
+++ CrewControl/src/connection.js
@@ -1,5 +1,9 @@
-import { Pool } from 'pg'; 
-import dotenv from 'dotenv'; 
+import pkg from 'pg'; 
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const { Pool } = pkg; 
 
 const pool = new Pool({
   user: process.env.DB_USER,
